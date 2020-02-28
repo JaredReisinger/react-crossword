3d5ad5bbda728b4b3e72d3c6991b9ac7f4c0c143
Jared Reisinger <jaredreisinger@hotmail.com>
2020-02-28 10:48:04 -0700

    refactor: factor grid/clue creation out and add tests

---
+++ src/Crossword/__test__/util.test.js
@@ -0,0 +1,110 @@
+// import React from 'react';
+// import ReactDom from 'react-dom';
+// import { render, cleanup } from '@testing-library/react';
+// import renderer from 'react-test-renderer';
+
+import '@testing-library/jest-dom/extend-expect';
+
+import {
+  byNumber,
+  calculateExtents,
+  createEmptyGrid,
+  fillClues,
+} from '../util';
+
+// afterEach(cleanup);
+
+const one = { number: '1' };
+const two = { number: '2' };
+
+it('byNumber returns 0 when a == b', () => {
+  const result = byNumber(one, one);
+  expect(result).toBe(0);
+});
+
+it('byNumber returns <0 when a < b', () => {
+  const result = byNumber(one, two);
+  expect(result).toBeLessThan(0);
+});
+
+it('byNumber returns >0 when a > b', () => {
+  const result = byNumber(two, one);
+  expect(result).toBeGreaterThan(0);
+});
+
+const simpleData = {
+  across: {
+    1: {
+      clue: 'one plus one',
+      answer: 'TWO',
+      row: 0,
+      col: 0,
+    },
+  },
+  down: {
+    2: {
+      clue: 'three minus two',
+      answer: 'ONE',
+      row: 0,
+      col: 2,
+    },
+  },
+};
+
+it('calculateExtents applies across length to col', () => {
+  const result = calculateExtents(simpleData, 'across');
+  expect(result).toEqual({ row: 0, col: 2 });
+});
+
+it('calculateExtents applies down length to row', () => {
+  const result = calculateExtents(simpleData, 'down');
+  expect(result).toEqual({ row: 2, col: 2 });
+});
+
+it('createEmptyGrid creates a row-major array', () => {
+  const result = createEmptyGrid(2);
+  expect(result[0][1]).toMatchObject({ row: 0, col: 1 });
+  expect(result[1][0]).toMatchObject({ row: 1, col: 0 });
+});
+
+it('fillClues can fill across', () => {
+  const gridData = createEmptyGrid(3);
+  const clues = { across: [] };
+
+  fillClues(gridData, clues, simpleData, 'across');
+
+  expect(gridData[0][0].used).toBeTruthy();
+  expect(gridData[0][0].answer).toBe('T');
+  expect(gridData[0][0].across).toBe('1');
+
+  expect(gridData[0][1].used).toBeTruthy();
+  expect(gridData[0][1].answer).toBe('W');
+  expect(gridData[0][1].across).toBe('1');
+
+  expect(gridData[0][2].used).toBeTruthy();
+  expect(gridData[0][2].answer).toBe('O');
+  expect(gridData[0][2].across).toBe('1');
+
+  expect(clues).toEqual({ across: [{ clue: 'one plus one', number: '1' }] });
+});
+
+it('fillClues can fill down', () => {
+  const gridData = createEmptyGrid(3);
+  const clues = { down: [] };
+
+  fillClues(gridData, clues, simpleData, 'down');
+
+  expect(gridData[0][2].used).toBeTruthy();
+  expect(gridData[0][2].answer).toBe('O');
+  expect(gridData[0][2].down).toBe('2');
+
+  expect(gridData[1][2].used).toBeTruthy();
+  expect(gridData[1][2].answer).toBe('N');
+  expect(gridData[1][2].down).toBe('2');
+
+  expect(gridData[2][2].used).toBeTruthy();
+  expect(gridData[2][2].answer).toBe('E');
+  expect(gridData[2][2].down).toBe('2');
+
+  expect(clues).toEqual({ down: [{ clue: 'two minus one', number: '2' }] });
+});
---
+++ src/Crossword/util.js
@@ -0,0 +1,121 @@
+const directionInfo = {
+  across: {
+    primary: 'col',
+    orthogonal: 'row',
+  },
+  down: {
+    primary: 'row',
+    orthogonal: 'col',
+  },
+};
+
+export function calculateExtents(data, direction) {
+  const dir = directionInfo[direction];
+  let primaryMax = 0;
+  let orthogonalMax = 0;
+
+  Object.entries(data[direction]).forEach(([i, info]) => {
+    const primary = info[dir.primary] + info.answer.length - 1;
+    if (primary > primaryMax) {
+      primaryMax = primary;
+    }
+
+    const orthogonal = info[dir.orthogonal];
+    if (orthogonal > orthogonalMax) {
+      orthogonalMax = orthogonal;
+    }
+  });
+
+  return {
+    [dir.primary]: primaryMax,
+    [dir.orthogonal]: orthogonalMax,
+  };
+}
+
+const emptyCellData = {
+  used: false,
+  number: null,
+  answer: '',
+  guess: '',
+  locked: false,
+  // row: r,
+  // col: c,
+  across: null,
+  down: null,
+};
+
+export function createEmptyGrid(size) {
+  const gridData = Array(size);
+  // Rather than [x][y] in column-major order, the cells are indexed as
+  // [row][col] in row-major order.
+  for (let r = 0; r < size; r++) {
+    gridData[r] = Array(size);
+    for (let c = 0; c < size; c++) {
+      gridData[r][c] = {
+        ...emptyCellData,
+        row: r,
+        col: c,
+      };
+    }
+  }
+
+  return gridData;
+}
+
+export function fillClues(gridData, clues, data, direction) {
+  const dir = directionInfo[direction];
+
+  Object.entries(data[direction]).forEach(([number, info]) => {
+    const { row: rowStart, col: colStart, clue, answer } = info;
+    for (let i = 0; i < answer.length; i++) {
+      const row = rowStart + (dir.primary === 'row' ? i : 0);
+      const col = colStart + (dir.primary === 'col' ? i : 0);
+      const cellData = gridData[row][col];
+
+      // TODO?: check to ensure the answer is the same if it's already set?
+      cellData.used = true;
+      cellData.answer = answer[i];
+      cellData[direction] = number;
+
+      if (i === 0) {
+        // TODO?: check to ensure the number is the same if it's already set?
+        cellData.number = number;
+      }
+    }
+
+    clues[direction].push({ number, clue });
+  });
+
+  clues[direction].sort(byNumber);
+}
+
+// Given the "nice format" for a crossword, generate the usable data optimized
+// for rendering and our interactivity.
+export function createGridData(data) {
+  const acrossMax = calculateExtents(data, 'across');
+  const downMax = calculateExtents(data, 'down');
+
+  const size =
+    Math.max(...Object.values(acrossMax), ...Object.values(downMax)) + 1;
+
+  const gridData = createEmptyGrid(size);
+
+  // Now fill with answers... and also collect the clues
+  const clues = {
+    across: [],
+    down: [],
+  };
+
+  fillClues(gridData, clues, data, 'across');
+  fillClues(gridData, clues, data, 'down');
+
+  return { size, gridData, clues };
+}
+
+// sort helper for clues...
+export function byNumber(a, b) {
+  const aNum = Number.parseInt(a.number, 10);
+  const bNum = Number.parseInt(b.number, 10);
+
+  return aNum - bNum;
+}
